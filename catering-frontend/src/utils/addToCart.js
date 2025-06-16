0790f81ffec06bfdda349056aeb474f4d756a03c
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2025-06-16 17:26:27 +0100

    feat: Refactor AddToCart function to accept data object for improved item addition

--- catering-frontend/src/utils/addToCart.js
+++ catering-frontend/src/utils/addToCart.js
@@ -1,17 +1,22 @@
 import { toast } from "react-toastify";
 import api from "../api";
 
-const AddToCart = (cart_code, dish, extra_item_id = null) => {
+const AddToCart = (cart_code, dish, data) => {
   const dish_id = dish.id;
   console.log(dish.name, dish_id )
-  const data = {
+  // const { quantity, orderOption, note, extras } = useContext(DishContext);
+  const { quantity, orderOption, note, xtras } = data;
+  const cartItemData = {
     cart_code,
     dish_id,
-    extra_item_id,
+    extra_items: xtras,
+    note,
+    orderoption: orderOption,
+    quantity,
   }
 
   //Fnctionality to allow user add item to cart
-  api.post('/api/cart/add_item/', data)
+  api.post('/api/cart/add_item/', cartItemData)
     .then(res => {
       console.log(res.data);
       localStorage.setItem(`cart_item_id_${dish.name}`, dish_id)
